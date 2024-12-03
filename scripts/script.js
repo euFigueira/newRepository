d14ddb94110a4db6776244af6993219d108a9582
Vinicius Figueira <figueiravinicius@hotmail.com>
2024-12-03 12:30:54 -0300

    primeiro commit

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const mobileNavbar = document.querySelector('.navbar_mobile')
+const button = document.querySelector('.burguer')
+
+button.addEventListener('click', function() {
+  mobileNavbar.classList.toggle('active')
+})
+
+
